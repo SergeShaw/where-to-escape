924bb2f8fd15123401ffdbe6460676c6130bf442
sergeshaw <sergeshaw@gmail.com>
2018-11-16 16:11:23 +0300

    radio question added

--- src/routing.js
+++ src/routing.js
@@ -5,6 +5,7 @@ import { bindActionCreators } from 'redux';
 import { loadData } from './data/ducks';
 import Question from './containers/Question';
 import Start from './containers/Start';
+import RadioQuestion from './containers/RadioQuestion';
 
 class Routing extends Component {
   componentDidMount() {
@@ -15,7 +16,18 @@ class Routing extends Component {
     return (
       <Switch>
         <Route exact path="/" component={Start} />
-        <Route exact path="/1" component={Question} />
+        <Route exact path="/1" render={() =>
+          <RadioQuestion
+            title="Хочешь, чтобы было тепло или это не важно?"
+            options={[
+              { text: "Хочу, чтобы было очень жарко" },
+              { text: "Мне не важно" },
+              { text: "Мне нравится когда холодно" },
+              { text: "Люблю умеренный климат" },
+            ]}
+          />
+        } />
+        <Route exact path="/2" component={Question} />
         <Route render={() => (<div>Miss</div>)} />
       </Switch>
     )
